8cd655ded613f102e8f568fb9310b866185da4c2
Joris De Winne <joris.dewinne@gmail.com>
2017-05-23 17:12:15 -0700

    Fixing code quality issues

--- src/main/resources/sonar/SonarSummaryTile.py
+++ src/main/resources/sonar/SonarSummaryTile.py
@@ -14,37 +14,28 @@ from xlrelease.HttpRequest import HttpRequest
 
 if not sonarServer:
     raise Exception("Sonar server ID must be provided")
-if not username:
-    username = sonarServer["username"]
-if not password:
-    password = sonarServer["password"]
 
-sonarUrl = sonarServer['url']
-
-credentials = CredentialsFallback(sonarServer, username, password).getCredentials()
-content = None
-RESPONSE_OK_STATUS = 200
-print "Sending content %s" % content
-sonarServerAPIUrl = '/api/measures/component?componentKey=%s&metricKeys=%s' % (resource, ','.join(metrics.keys()))
-http_request = HttpRequest(sonarServer, credentials['username'], credentials['password'])
-sonarResponse = http_request.get(sonarServerAPIUrl)
-if sonarResponse.status == RESPONSE_OK_STATUS:
-    json_data = json.loads(sonarResponse.getResponse())
+sonar_url = sonarServer['url']
+sonar_server_api_url = '/api/measures/component?componentKey=%s&metricKeys=%s' % (resource, ','.join(metrics.keys()))
+http_request = HttpRequest(sonarServer, username, password)
+sonar_response = http_request.get(sonar_server_api_url)
+if not sonar_response.isSuccessful():
+    json_data = json.loads(sonar_response.getResponse())
     data1 = {}
     data1['id'] = json_data['component']['id']
     data1['key'] = json_data['component']['key']
     data1['name'] = json_data['component']['name']
-    data1['sonarUrl'] = sonarUrl
+    data1['sonarUrl'] = sonar_url
     for item in json_data['component']['measures']:
         data1[item['metric']] = item['value']
     data = data1
 else:
-    error = json.loads(sonarResponse.getResponse())
+    error = json.loads(sonar_response.getResponse())
     if 'Invalid table' in error['error']['message']:
         print "Invalid Table Name"
         data = {"Invalid table name"}
-        sonarResponse.errorDump()
+        sonar_response.errorDump()
     else:
         print "Failed to run query in Sonar"
-        sonarResponse.errorDump()
-        sys.exit(1)
+        sonar_response.errorDump()
+    sys.exit(1)
